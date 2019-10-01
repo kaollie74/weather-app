72977dc3a68648d7c4f88d29e1d8ee233a38db50
kaollie74 <kaollie74@gmail.com>
2019-10-01 09:51:11 -0500

    created titles component and imported into App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// import components
+import Titles from '../Titles/Titles';
+
 class App extends Component {
   
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
+        <Titles/>
       </div>
     );
   }
