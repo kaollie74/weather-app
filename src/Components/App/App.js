e9a8811910ebfc1f0bc5e87a5348de98e6238c2d
kaollie74 <kaollie74@gmail.com>
2019-10-01 10:56:18 -0500

    caputered input values and able to pass up to function getWeather()

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-require('dotenv').config();
-const WEATHER_API_KEY= process.env.WEATHER_API_KEY;
-
-
-
 // import components
 import Form from '../Form/Form';
 import Titles from '../Titles/Titles';
 import Weather from '../Weather/Weather';
 
+//import api key
+require('dotenv').config();
+const WEATHER_API_KEY= '9ea676ae955d21c9805b7986312a2652';
+
+
+
+
+
+
 
 class App extends Component {
 
-  componentDidMount () {
-    this.getWeather();
-  }
+  // componentDidMount () {
+  //   this.getWeather();
+  // }
   
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${WEATHER_API_KEY}
+  getWeather = async (items) => {
+
+  
+    console.log('in get weather', items)
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q='${items.city},${items.country}'&APPID=${WEATHER_API_KEY}
     `);
 
     const data = await api_call.json();
@@ -31,7 +39,7 @@ class App extends Component {
       <div className="App">
         <h1>Hello</h1>
         <Titles/>
-        <Form/>
+        <Form getWeather= {this.getWeather}/>
         <Weather/>
       </div>
     );
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
+  state = {
+    city: '',
+    country: ''
+  }
+
+  handlChange = (event, propsName) => {
+    this.setState({
+      [propsName]: event.target.value
+    })
+  }
 
   render (){
+
+    console.log('This is State', this.state)
+
     return (
-      <form>
-        <input type='text' name='city' placeholder='city..'/>
-        <input type='text' name='country' placeholder='Country...'/>
-      </form>
+      <div>
+        <input type='text' name='city' placeholder='city..' onChange= {(event) => this.handlChange(event, 'city')}/>
+        <input type='text' name='country' placeholder='Country...' onChange= {(event) => this.handlChange(event, 'country')} />
+        <button onClick= {() => this.props.getWeather(this.state)}>Get Weather</button>
+
+      </div>
     )
   }
 
